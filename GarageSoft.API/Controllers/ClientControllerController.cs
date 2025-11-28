99af5d6d4aad08bd8e7be198004fad5c36343941
rojo1585 <jorge.mendoza1585@outlook.com>
2025-11-27 20:51:28 -0600

    add test controller

---
+++ GarageSoft.API/Controllers/ClientControllerController.cs
@@ -0,0 +1,18 @@
+﻿using Azure;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GarageSoft.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientControllerController(IMediator _mediator) : ControllerBase
+    {
+        [HttpGet("{clientId}")]
+        public async Task<ActionResult<Response<int?>>> GetJustBalnce(int clientId)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ GarageSoft.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace GarageSoft.Application;
+
+public static class ApplicationServiceRegistration
+{
+    extension(IServiceCollection source)
+    {
+        public IServiceCollection AddApplicationServices()
+        {
+
+            source.AddAutoMapper(cfg => { cfg.AddMaps(AppDomain.CurrentDomain.GetAssemblies()); });
+            source.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            return source;
+        }
+    }
+}
